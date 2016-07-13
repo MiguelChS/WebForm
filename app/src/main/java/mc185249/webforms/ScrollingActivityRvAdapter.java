4f8f47dcfccc576ab2614aaa42024af085b97ec7
Joaquin Noe <joako.n96@gmail.com>
2016-07-13 11:22:39 -0300

    Fix bug al intentar abrir un nuevo form

--- app/src/main/java/mc185249/webforms/ScrollingActivityRvAdapter.java
+++ app/src/main/java/mc185249/webforms/ScrollingActivityRvAdapter.java
@@ -47,38 +47,48 @@ public class ScrollingActivityRvAdapter extends RecyclerView.Adapter<ScrollingAc
                 switch (model.getText(position)){
                     case "Inventario Partes":
                         Intent i = new Intent(mContext, InventoryActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(i);
                         break;
                     case "Environmental Site":
                         Intent intent = new Intent(mContext,EnvironmentalSiteActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(intent);
+
                         break;
                     case "Logistics Survey":
                         Intent intent1 = new Intent(mContext,LogisticsSurveyActivity.class);
+                        intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(intent1);
                         break;
                     case "Mantenimiento":
                         Intent mantenimientoIntent = new Intent(mContext, MantenimientoSurveyActivity.class);
+                        mantenimientoIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(mantenimientoIntent);
                         break;
                     case "Memoria Fiscal":
                         Intent in = new Intent(mContext, memoriaFiscalActivity.class);
+                        in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(in);
                         break;
                     case "Cambio Pid Pad":
                         Intent intentt1 = new Intent(mContext,cambioPidPad.class);
+                        intentt1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(intentt1);
                         break;
                     case "Visita Tecnica":
                         Intent intent2 = new Intent(mContext,VisitaTecnica.class);
+                        intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(intent2);
                         break;
                     case "Teclado Encryptor":
                         Intent intent3 = new Intent(mContext,TecladoEncryptorActivity.class);
+                        intent3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(intent3);
                         break;
                     case "Devolucion Partes":
                         Intent intent4 = new Intent(mContext, DevolucionPartesActivity.class);
+                        intent4.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(intent4);
                         break;
                 }
